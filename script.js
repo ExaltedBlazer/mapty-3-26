fc57ac668b6433401ffc38f938d53dff5d747253
Kaden Horrell-St.Cloud <death5819@gmail.com>
2026-03-20 20:34:59 -0400

    Fourth commit

--- script.js
+++ script.js
@@ -11,92 +11,95 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
-
 class App {
+  #map;
+  #mapEvent;
+
   constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
   }
 
+//================= _getPosition() method =================//
   _getPosition() {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+    if(navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
       function () {
       alert('Could not get your position');
     }
   );
 }
-  }
-
+//================= _loadMap() method =================//
   _loadMap(position) {
-    
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
-      map = L.map('map').setView(coords, 15);
+      if (this.#map != undefined && this.#map != null) {
+        this.#map.remove();
+      }
 
-      console.log(map);
+      this.#map = L.map('map').setView(coords, 15);
+      //console.log(map);
       
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+      }).addTo(this.#map);
 
       L.marker(coords)
-        .addTo(map)
+        .addTo(this.#map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
 
       // Handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
+      this.#map.on('click', this._showForm.bind(this));
+  }
+//================= _showForm() method =================//
+  _showForm(mapE) {
+     this.#mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
-      });
-  }
-  
-  _showForm() {
 }
+//================= _toggleElevationField() method =================//
   _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
 }
-  _newWorkout() {
-}
-}
-//=======================================================// APPLICATION ARCHITECTURE
-
-
-  form.addEventListener('submit', function (e) {
+//================= _newWorkout() method =================//
+  _newWorkout(e) {
     e.preventDefault();
 
     // Clear input fields
     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
     // Display marker
-
-    console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(L.popup(
-            { 
-            maxWidth: 250, 
-            minWidth: 100, 
-            autoClose: false, 
-            closeOnClick: false,
-            className: 'running-popup' 
-            }
-          ))
-          .setPopupContent('Workout')
-          .openPopup();
-  });
-
-  inputType.addEventListener('change', function () {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-    
-  });
+    const { lat, lng } = this.#mapEvent.latlng;
+      L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(L.popup(
+        { 
+          maxWidth: 250, 
+          minWidth: 100, 
+          autoClose: false, 
+          closeOnClick: false,
+          className: 'running-popup' 
+        }
+      ))
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
+
+const app = new App();
+app._getPosition();
+
+//=======================================================// APPLICATION ARCHITECTURE
+
+
+  
